aa3f6e69a4f5448885f25a9ce0c42ada2fad7597
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-07-24 22:06:39 -0400

    prueba

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+const process = require('process');
+const pathN = require('path');
+
+let path = process.argv[1]
+
+console.log(path)
