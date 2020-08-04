376dbc2557c60a441598a5842d2a8ad368714acf
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-08-04 09:48:29 -0400

    ok

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 const chalk = require("chalk");
 const mdLinks = require("./md-links.js");
 //variable global
-let path = process.argv[2];
+let path = process.argv[2]
+ // path.resolve para convertir en absoluta
+path = pathNode.resolve(path);
+ // path.normalize para normalizar el path en caso se que hayan errores de semántica
+path = pathNode.normalize(path);
+
 let options = {
   stats: false,
   validate: false,
 }
- process.argv.forEach(val =>{  //option
- if( val == "--stats" || val == "-s"){
+
+process.argv.forEach(element =>{
+ if( element == "--stats" || element == "--s" ){
    options.stats = true
  }
-if(val == "--validate" || val == "-v"){
+if(element == "--validate" || element == "--v"){
   options.validate = true
 }
 })
-mdLinks.mdLinks(path,options).then(res=>{
+
+mdLinks.mdLinks(path,options).then(res => {
+  if(options.validate && options.stats){
+    return console.log(chalk.bgBlue("Total Links: " + res.total) + "\n" + chalk.bgGreen("Ok Links: " + res.ok) + "\n" + chalk.bgRed("Broken Links: " + res.broken))
+  }
   if(options.validate){
     if(res.length === 0){
       return console.log(chalk.red("No se encontraron links"))
-    }else{
-      let validateLinks = res.map(x=>x.file+"  "+ chalk.yellow(x.text.substr(0,40))+ " " + chalk.blue(x.href) +"  "+ chalk.bgGreen(x.status) + " "+ chalk.bgMagenta(x.statusCode))
-      return console.log(validateLinks.join("\n "))
     }
-  }else if(options.stats){
-    return console.log(chalk.bgBlue("Total Links: "+ res.total)+"\n"+chalk.bgGreen("Unique Links: "+res.unique))
+    let validateLinks = res.map(element => chalk.bgBlack(element.file) +"  " + chalk.yellowBright(element.href) + "  " + chalk.cyanBright(element.text.substr(0,50)) + "  " + chalk.bgMagenta(element.status) + " " + chalk.bgGreen(element.statusCode))
+    return console.log(validateLinks.join("\n "))
+  }
+  if(options.stats){
+    return console.log(chalk.bgBlue("Total Links: "+ res.total) + "\n" + chalk.bgMagenta("Unique Links: "+res.unique))
   }else{
-    if(options.validate && options.stats){
-      return console.log(chalk.bgBlue("Total Links: " + res.total) + "\n" + chalk.bgGreen("Ok Links: " + res.ok) + "\n" + chalk.bgRed("Broken Links: " + res.broken))
-    }else{
-      if(res.length === 0){
-        return console.log(chalk.red("No se encontraron links"))
-      }else{
-        const resLinks = res.map(element => element.file + "  " + chalk.yellowBright(element.href) + "  " + chalk.cyanBright(element.text.substr(0,50)))
-        return console.log(resLinks.join("\n "))
-      }
+    if(res.length === 0){
+      return console.log(chalk.red("No se encontraron links"))
+    }
+    const resLinks = res.map(element => element.file + "  " + chalk.yellowBright(element.href) + "  " + chalk.cyanBright(element.text.substr(0,50)))
+    return console.log(resLinks.join("\n "))
   }
-  }
-  }).catch(err=>{
-    console.log(chalk.red(err.message))
-  });
+}).catch(err => {
+  console.log(chalk.red(err.message))
+});
